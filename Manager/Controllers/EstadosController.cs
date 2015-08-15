396320003f526ff7c24a9a89662f0d9430c6e50f
Huakus <joa.611@gmail.com>
2015-08-15 16:38:49 -0300

    Agregando funcionalidades.

---
+++ Manager/Controllers/EstadosController.cs
@@ -0,0 +1,92 @@
+﻿using Manager.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Manager.Controllers
+{
+    public class EstadosController : Controller
+    {
+        HalconDBEntities db = new HalconDBEntities();
+
+        // GET: Estados
+        public ActionResult Index()
+        {
+            return View(db.Estados.ToList());
+        }
+
+        // GET: Estados/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Estados/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Estados/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Estados/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Estados/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Estados/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Estados/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
