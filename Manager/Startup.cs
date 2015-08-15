3db9c798ff84450fc50f967fbfb89e01277a8892
Huakus <joa.611@gmail.com>
2015-08-15 15:20:37 -0300

    Creación del proyecto

---
+++ Manager/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Manager.Startup))]
+namespace Manager
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
