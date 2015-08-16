a47cf49cd8e8ab20d4c196733f363aebe167e1fd
Huakus <joa.611@gmail.com>
2015-08-16 03:11:48 -0300

    Agregando nuevas tablas.

---
+++ Manager/Controllers/UsuariosController.cs
@@ -0,0 +1,91 @@
+﻿using Manager.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Manager.Controllers
+{
+    public class UsuariosController : Controller
+    {
+        HalconDBEntities db = new HalconDBEntities();
+        // GET: Usuarios
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Usuarios/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Usuarios/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Usuarios/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Usuarios/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Usuarios/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Usuarios/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Usuarios/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
